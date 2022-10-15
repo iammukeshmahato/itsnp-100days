210647829515fc0228c01b7809a39dc29c3d1f2d
Nishant Shah <99190466+nishantshah977@users.noreply.github.com>
2022-10-15 16:59:43 +0545

    Create script.js

---
+++ audio-player/script.js
@@ -0,0 +1,25 @@
+var audio = document.getElementById('audio');  
+var title = document.getElementById("title");
+let song ={
+    peaches:"https://connectloaded.com/wp-content/uploads/2021/03/Justin-Bieber-Peaches-ft.-Daniel-Caesar-Giveon.mp3?_=3",
+whatdoyoumean: "https://muxicplays.com/wp-content/uploads/2022/07/Justin-Bieber-What-Do-You-Mean.mp3?_=1",
+};    
+function pausesong(){
+    audio.pause();
+}
+function nextsong(){
+     title.innerText="What Do you Mean"
+    audio.src=song.whatdoyoumean;
+    audio.play();
+}
+function previoussong(){
+title.innerText="Peaches";
+    audio.src=song.peaches;
+    audio.play;
+}
+
+
+
+function playsong(){
+    audio.play();   
+}
