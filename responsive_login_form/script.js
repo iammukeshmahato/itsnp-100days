238807b1ca23c9cc6278d2589c5c4766cd8f28af
Nishant Shah <99190466+nishantshah977@users.noreply.github.com>
2022-10-30 12:37:55 +0545

    Update script.js

--- responsive_login_form/script.js
+++ responsive_login_form/script.js
@@ -4,16 +4,13 @@ let cpass = document.getElementById('cpass').value;
 let error = document.getElementById('error');
 let form = document.getElementById('form');
     if(pass.length<4){ 
-     error.innerText="Password must be 4 Character or more";        
+     error.innerText="Password must be 4 Character or more";          
     }
 let error1 = document.getElementById('error1');    
     if(pass!= cpass){
-      error1.innerText="Password do not match";   
-        
+      error1.innerText="Password do not match"; 
     }
    if(pass.length>=4 && pass==cpass) {
-       form.submit();
-       alert("Submitted ");
-      
+      alert("Just Demo Project");
    }
 }
